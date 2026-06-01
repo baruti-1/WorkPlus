6b095b68063141cd8d999f254e3e2acdeeb07a09
baruti-1 <barutibaruani1@gmail.com>
2026-06-01 12:19:06 +0300

    Create Hero component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,12 +8,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>{{ $title ?? 'WorkPlus | Find and List Jobs' }}</title>
     @vite('resources/css/app.css')
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
